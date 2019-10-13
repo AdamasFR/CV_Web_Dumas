b9213586c069a13ceab61bde07c1b677b151a279
Anthony Dumas <anthony@dumas.cc>
2019-10-13 15:41:29 +0000

    Test fichier deploy

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,12 @@ LOCAL=$(git rev-parse @)
 REMOTE=$(git rev-parse "$UPSTREAM")
 BASE=$(git merge-base @ "$UPSTREAM")
 
-if [ $LOCAL = $BASE ]; then
-    git stash
-    git pull
-    npm install
+if [ $LOCAL = $REMOTE ]; then
+    echo "Up-to-date"
+elif [ $LOCAL = $BASE ]; then
+    echo "Need to pull"
+elif [ $REMOTE = $BASE ]; then
+    echo "Need to push"
+else
+    echo "Diverged"
 fi
