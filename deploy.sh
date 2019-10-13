a6a573dfeb2184094c29d4e170c54c6f65a67f8c
Anthony Dumas <anthony@dumas.cc>
2019-10-13 14:36:36 +0000

    Ajout d'un script de deploy

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+UPSTREAM=${1:-'@{u}'}
+LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse "$UPSTREAM")
+BASE=$(git merge-base @ "$UPSTREAM")
+
+if [ $LOCAL = $BASE ]; then
+    git stash
+    git pull
+    npm install
+fi
