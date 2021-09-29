52cf674651ea14791eab540a691bb775102b2f88
Anthony Dumas <anthony@dumas.cc>
2021-09-30 00:15:56 +0200

    Multiple ajout de Target Blank

--- src/components/ExperienceCard/ExperienceCard.js
+++ src/components/ExperienceCard/ExperienceCard.js
@@ -16,7 +16,7 @@ export default class ExperienceCard extends Component {
       return <div class="card-content">
       <div class="media">
           <div class="media-left">
-              <a href={data.url}>
+              <a href={data.url} target="_blank">
                   <figure class="image is-48x48">
                       <img src={imgSrc} alt="Logo Entreprise"/>
                   </figure>
