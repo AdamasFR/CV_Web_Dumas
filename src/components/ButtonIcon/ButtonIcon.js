5e361660f0d2baa2b27f4dc3c2d60cc63f87caa7
Anthony Dumas <anthony@dumas.cc>
2022-06-27 23:38:19 +0200

    Fix Buttons

--- src/components/ButtonIcon/ButtonIcon.js
+++ src/components/ButtonIcon/ButtonIcon.js
@@ -1,17 +1,22 @@
-import React, {Component} from '../../../node_modules/react';
-import './ButtonIcon.scss'
+import './ButtonIcon.scss';
+
+import React, { Component } from '../../../node_modules/react';
+
 
 export default class ButtonIcon extends Component {
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {};
-    // }
-    render() {
-      return <a class="button" rel="noopener noreferrer" href={this.props.lien} target="_blank"
-            aria-label="Profil {this.props.icon}">
-          <span class="icon is-large">
-              <ion-icon name={this.props.icon}></ion-icon>
-          </span>
-      </a>;
-    }
+  // constructor(props) {
+  //     super(props);
+  //     this.state = {};
+  // }
+  render() {
+    return <a rel="noopener noreferrer" href={this.props.lien} target="_blank"
+      aria-label="Profil {this.props.icon}">
+      <button class="button">
+        <span class="icon">
+          <ion-icon name={this.props.icon}></ion-icon>
+        </span>
+      </button>
+    </a>;
+
   }
+}
--- src/components/MainCard/MainCard.js
+++ src/components/MainCard/MainCard.js
@@ -1,85 +1,91 @@
-import React, {Component} from '../../../node_modules/react';
-import {FormattedMessage} from 'react-intl';
-import { injectIntl } from 'react-intl'
+import './MainCard.scss';
 
-import ExperienceCard from '../ExperienceCard/index';
-import ButtonIcon from '../ButtonIcon/index';
+import { FormattedMessage, injectIntl } from 'react-intl';
 
-import './MainCard.scss'
-import headshotMain from '../../images/headshot_main.png';
+import React, { Component } from '../../../node_modules/react';
 import headshotFond from '../../images/headshot_fond.png';
+import headshotMain from '../../images/headshot_main.png';
+import ButtonIcon from '../ButtonIcon/index';
+import ExperienceCard from '../ExperienceCard/index';
+
 
 class MainCard extends Component {
     render() {
         var metaThemeColor = document.querySelector("meta[name=theme-color]");
-        metaThemeColor.setAttribute("content", window.matchMedia('(prefers-color-scheme: dark)').matches ?"#25444b" : "#5c9ead");
+        metaThemeColor.setAttribute("content", window.matchMedia('(prefers-color-scheme: dark)').matches ? "#25444b" : "#5c9ead");
         const experienceCardMessage = this.props.intl.messages.experienceCard;
-      return <section id="idCard" class="hero is-medium is-primary-inverted is-bold">
-        <div class="hero-body">
-          <div class="container">
-              <div class="columns">
-                  <div class="column is-2 is-hidden-touch is-hidden-desktop-only dontPrint"></div>
-                  <div id="photosAndButtons" class="column is-3-widescreen is-4-tablet">
-                      <div class="card">
-                          <div class="card-image">
-                              <figure id="photoProfil" class="image">
-                                  <img class="background" src={headshotFond} alt="Anthony Dumas"/>
-                                  <img class="actor" src={headshotMain} alt="Anthony Dumas"/>
-                              </figure>
-                          </div>
-                          <div class="card-content">
-                              <div class="media">
-                                  <div class="media-content">
-                                      <p id="buttonsUnderPhoto" class="buttons are-large ">
-                                          <ButtonIcon 
-                                          icon="logo-github"
-                                          lien="https://github.com/AdamasFR"/>
+        return <section id="idCard" class="hero is-medium is-primary-inverted is-bold">
+            <div class="hero-body">
+                <div class="container">
+                    <div class="columns">
+                        <div class="column is-2 is-hidden-touch is-hidden-desktop-only dontPrint"></div>
+                        <div id="photosAndButtons" class="column is-3-widescreen is-4-tablet">
+                            <div class="card">
+                                <div class="card-image">
+                                    <figure id="photoProfil" class="image">
+                                        <img class="background" src={headshotFond} alt="Anthony Dumas" />
+                                        <img class="actor" src={headshotMain} alt="Anthony Dumas" />
+                                    </figure>
+                                </div>
+                                <div class="card-content">
+                                    <div class="media">
+                                        <div class="media-content">
+                                            <p id="buttonsUnderPhoto" class="field is-grouped">
+                                                <p class="control">
+                                                    <ButtonIcon
+                                                        icon="logo-github"
+                                                        lien="https://github.com/AdamasFR" />
+                                                </p>
+                                                <p class="control">
+                                                    <ButtonIcon
+                                                        icon="logo-linkedin"
+                                                        lien="https://www.linkedin.com/in/anthonyyvondumas/" />
+                                                </p>
+                                                <p class="control">
+                                                    <ButtonIcon
+                                                        icon="logo-twitter"
+                                                        lien="https://www.twitter.com/adamas_fr" />
+                                                </p>
+                                            </p>
+                                            <p id="boutonCV" class="field">
+                                                <a rel="noreferrer noopener" target="_blank"
+                                                    href="https://files.adm.ink/anthony-dumas-cv.pdf" >
+                                                    <button class="button is-fullwidth">
+                                                        <span class="icon">
+                                                            <i class="fab icon-file-pdf-o"></i>
+                                                        </span>
+                                                        <span><FormattedMessage id="curriculumVitaeLink" /></span>
+                                                    </button>
+                                                </a>
+                                            </p>
+                                        </div>
+                                    </div>
 
-                                          <ButtonIcon
-                                          icon="logo-linkedin"
-                                          lien="https://www.linkedin.com/in/anthonyyvondumas/"/>
+                                    <div class="content">
 
-                                          <ButtonIcon
-                                          icon="logo-twitter"
-                                          lien="https://www.twitter.com/adamas_fr"/>
-                                      </p>
-                                      <p id="boutonCV" class="buttons are-large">
-                                          <a class="button is-fullwidth" rel="noreferrer noopener" target="_blank"
-                                              href="https://files.adm.ink/anthony-dumas-cv.pdf" >
-                                              <span class="icon">
-                                                  <i class="fab icon-file-pdf-o"></i>
-                                              </span>
-                                              <span><FormattedMessage id="curriculumVitaeLink"/></span>
-                                          </a>
-                                      </p>
-                                  </div>
-                              </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
 
-                              <div class="content">
-
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-                  
-                  <div class="column is-9-tablet is-7-widescreen" id="experiences">
-                      <span class="title">{experienceCardMessage.title.experience}</span>
-                      {experienceCardMessage.experiences.map(function(experience, index){
-                    return <ExperienceCard 
-                    data={experience}/>;
-                  })}
-                    <span class="title">{experienceCardMessage.title.education}</span>
-                    {experienceCardMessage.education.map(function(education, index){
-                    return <ExperienceCard 
-                    data={education}/>;
-                  })}
-                  </div>
-              </div>
-          </div>
-          </div>
-  </section>;
+                        <div class="column is-9-tablet is-7-widescreen" id="experiences">
+                            <span class="title">{experienceCardMessage.title.experience}</span>
+                            {experienceCardMessage.experiences.map(function (experience, index) {
+                                return <ExperienceCard
+                                    data={experience} />;
+                            })}
+                            <span class="title">{experienceCardMessage.title.education}</span>
+                            {experienceCardMessage.education.map(function (education, index) {
+                                return <ExperienceCard
+                                    data={education} />;
+                            })}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>;
     }
-  }
+}
 
 
 export default injectIntl(MainCard)
