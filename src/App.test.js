86c0df290d4d25bb29dc9c429d861a6ae9470d13
Anthony Dumas <anthony@dumas.cc>
2023-03-27 23:26:27 +0200

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Anthony Yvon Dumas', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Anthony Yvon Dumas/i);
   expect(linkElement).toBeInTheDocument();
 });
